72806b8af316f11b513d62608fd8e4123a93457c
sz9ddnbd4z-tech <sz9ddnbd4z@privaterelay.appleid.com>
2026-09-09 00:11:05 +0200

    chore: include content layer in offline shell

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='budapeszt-v13';
-const SHELL=['./','./index.html','./app-v8.html?v=20260909a','./app-v5.html?v=20260908g','./fix-ios.js?v=20260909a','./manifest.webmanifest','./icon-180.png','./icon.svg'];
+const C='budapeszt-v14';
+const SHELL=['./','./index.html','./app-v8.html?v=20260909a','./app-v5.html?v=20260908g','./fix-ios.js?v=20260909a','./content-v2.js?v=20260909a','./manifest.webmanifest','./icon-180.png','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
